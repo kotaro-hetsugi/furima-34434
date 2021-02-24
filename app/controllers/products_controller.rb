92631e7caf1e48151a27d78e31217b960b7a9cad
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-02-24 19:21:51 +0900

    商品出品機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,19 @@ class ProductsController < ApplicationController
   end
 
   def create
-    
+    product = Product.create(product_params)
+    if product.save
+      redirect_to root_path
+    else
+      @product = Product.create(product_params)
+      render :new
+    end
+  end
+
+
+  private
+  
+  def product_params
+    params.require(:product).permit(:image, :title, :text, :category_id, :status_id, :delivery_fee_role_id, :delivery_source_area_id, :delivery_days_id, :price).merge(user_id:current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -17,6 +17,7 @@ class Product < ApplicationRecord
   validates :delivery_days_id,        numericality: { other_than: 1 }
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates :price, presence: true,format:{with: PRICE_REGEX, message:'Half-width number' }
+  validates :price, numericality: true, inclusion: {in: 300..999999999 , message:'Out of setting range'}
 
   belongs_to :user
   has_one :buy
