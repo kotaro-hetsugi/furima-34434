5b7eb71b82fbb28d30cb3d2495913a5f1cec4d47
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-02-24 15:26:11 +0900

    値段の正規表現設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,8 @@ class Product < ApplicationRecord
   validates :delivery_fee_role_id,    presence: true
   validates :delivery_source_area_id, presence: true
   validates :delivery_days_id,        presence: true
-  validates :price,                   presence: true
+  PRICE_REGEX = /\A[0-9]+\z/.freeze
+  validates :price, presence: true,format:{with: PRICE_REGEX, message:'Half-width number' }
 
   belongs_to :user
   has_one :buy
