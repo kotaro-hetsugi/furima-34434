36873fda1f51f663be51071b19faa2adf91fec6e
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-02-24 15:11:20 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+  validates :image,                   presence: true
+  validates :title,                   presence: true
+  validates :text,                    presence: true
+  validates :category_id,             presence: true
+  validates :status_id,               presence: true
+  validates :delivery_fee_role_id,    presence: true
+  validates :delivery_source_area_id, presence: true
+  validates :delivery_days_id,        presence: true
+  validates :price,                   presence: true
+
   belongs_to :user
   has_one :buy
   has_one_attached :image
