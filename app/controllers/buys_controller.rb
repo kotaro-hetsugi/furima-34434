1d0d613de3b72f6e05b2961ada1c2dde34c70bd9
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-02-27 22:53:09 +0900

    createアクション作成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,4 +2,8 @@ class BuysController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
   end
+
+  def create
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
   resources :products do
-    resources :buys, only: [:index]
+    resources :buys, only: [:index, :create]
   end
 end
