2e5857ae7f7152a3dcc3ff2109c98d25372aaaec
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-02-28 10:15:10 +0900

    インスタンスの生成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,18 @@
 class BuysController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
+    @buy = Buy.new
   end
 
   def create
+    #buy = Buy.new(buy_params)
+  end
 
+
+  private
+
+  def buy_params
+    # params.require(:buy).permit(:pice)
+     # :postal_code, :delivery_source_area_id, :municipality,  :address, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
