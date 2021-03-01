5a7419a0a2ea094fb41bbf37828382b662366ad7
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-01 12:24:17 +0900

    自身投稿商品の購入ページ遷移防止

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,9 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @product = Product.find(params[:product_id])
-    #@buy = Buy.new
     @buy_buyer = BuyBuyer.new
   end
 
@@ -23,6 +23,13 @@ class BuysController < ApplicationController
 
   def buy_params
     params.require(:buy_buyer).permit(:postal_code, :prefectures_id, :municipality,  :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+  end
+
+  def move_to_index
+    product = Product.find(params[:product_id])
+    unless product.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 
 end
